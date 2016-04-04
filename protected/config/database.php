1aad10f3eabc530a543c807f6786c07e4ebb7c55
jvictorsk8er <jvictorsilva@outlook.com>
2016-04-04 20:56:16 -0300

    teste

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=gerenciargov',
+	'connectionString' => 'mysql:host=localhost;dbname=gerenciargo',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
