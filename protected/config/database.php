e4cc8fed5671b00114d389e670ab7b91f852a2d1
jvictorsk8er <jvictorsilva@outlook.com>
2016-04-12 23:08:46 -0300

    configuracao data base

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=gerenciargov',
+	'connectionString' => 'mysql:host=192.185.210.67;dbname=geren759_gerenciargov',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'geren759_lucas',
+	'password' => 'admin123@',
 	'charset' => 'utf8',
 	
 );
