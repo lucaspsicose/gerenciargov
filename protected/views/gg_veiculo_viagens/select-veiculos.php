e74d145d18e125fc44ca262847602682aa77a22e
Lucas Simões <lucas_psicose@hotmail.com>
2016-05-03 00:04:12 -0300

    Commit resolução do caso 29 do mantis

---
+++ protected/views/gg_veiculo_viagens/select-veiculos.php
@@ -0,0 +1,76 @@
+<?php
+    $veiculos = Gg_veiculos::model()->findAll(array('order'=>'veiculo_descricao'));
+    $this->layout = '//layouts/column1';
+    
+    Yii::app()->clientScript->registerScript('tooltip', "$(document).ready(function(){
+        $('[data-toggle='tooltip']').tooltip();   
+        });");
+?>
+
+<div class="white-bg">
+    <div class="container career-inner">
+        <div class="row">
+            <div class="col-md-12 career-head">
+                <h1 class="wow fadeIn">Viagens - Selecionar Veículo</h1>                
+            </div>
+            <hr>
+            <div class="box wow fadeIn">
+                <div class="">
+                    <?php foreach ($veiculos as $veiculo) {
+                        switch ($veiculo['veiculo_tipo']) {
+                            case 1: 
+                                $tipo = 'fa-car';
+                                break;
+                            case 2:
+                                $tipo = 'fa-taxi';
+                                break;
+                            case 3:
+                                $tipo = 'fa-car';
+                                break;
+                            case 4:
+                                $tipo = 'fa-bus';
+                                break;
+                            case 5:
+                                $tipo = 'fa-bus';
+                                break;
+                            case 6:
+                                $tipo = 'fa-ambulance';
+                                break;
+                            case 7:
+                                $tipo = 'fa-truck';
+                                break;
+                            case 8:
+                                $tipo = 'fa-train';
+                                break;
+                            case 9:
+                                $tipo = 'fa-bicycle';
+                                break;
+                        } 
+                        
+                        switch ($veiculo['status_veiculos_id']) {
+                            case 1:
+                                $cor = '#DFF0D8';
+                                $status = 'Disponível';
+                                break;
+                            case 2:
+                                $cor = '#F2DEDE';
+                                $status = 'Em Uso';
+                                break;
+                            case 3:
+                                $cor = '#FCF8E3';
+                                $status = 'Manutenção';
+                                break;
+                        }
+                        
+                        echo '<a href="'.$this->createUrl('gg_veiculo_viagens/create', array('veiculo'=>$veiculo['veiculos_id'])).'" data-toggle="tooltip" title="'.$status.'" id="'.$veiculo['veiculos_id'].'" class="btn btn-veiculo" style="background-color: '.$cor.'">
+                                 <i class="fa '.$tipo.'" aria-hidden="true"></i>'.wordwrap($veiculo['veiculo_descricao'], 15, "<br />\n").'<br />
+                                 '.$veiculo['veiculo_placa'].'    
+                             </a>';
+                    } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -13,7 +13,8 @@
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/css/theme.css" rel="stylesheet">
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/css/bootstrap-reset.css" rel="stylesheet">
     <!--external css-->
-    <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <!--<link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />-->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/css/flexslider.css"/>
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/assets/assets/bxslider/jquery.bxslider.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/css/animate.css">
